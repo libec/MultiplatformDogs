46d4a25b28a7d8800e4622969512b8af86d42d64
Libec <libor.huspenina@gmail.com>
2022-02-03 13:15:52 +0100

    Move BreedsRepository implementation to its own file

---
+++ Dogs/Sources/Dogs/Features/Breeds/Domain/BreedsLocalRepository.swift
@@ -0,0 +1,31 @@
+import Combine
+
+public final class BreedsLocalRepository: BreedsRepository {
+
+    private let breedsResource: BreedsResource
+
+    private let subject = CurrentValueSubject<[Breed], Never>([])
+    private var subscriptions = Set<AnyCancellable>()
+
+    public init(breedsResource: BreedsResource) {
+        self.breedsResource = breedsResource
+    }
+
+    public func fetch() {
+        breedsResource.fetch()
+            .replaceError(with: [])
+            .sink { [weak self] breed in
+                guard let unwrappedSelf = self else { return }
+                unwrappedSelf.subject.send(breed)
+            }.store(in: &subscriptions)
+    }
+
+    public var query: AnyPublisher<[Breed], Never> {
+        subject
+            .eraseToAnyPublisher()
+    }
+
+    public var last: [Breed] {
+        subject.value
+    }
+}
